f169e0a51075df0741bab25041f30bef9eb0851b
Nicolas De Lauretis <nicolas_villagesel@hotmail.com>
2025-11-14 10:10:50 -0300

    Fix: fixes on wallpapar files

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ cd "$(dirname "$0")" || exit 1
 
 echo "Installing dotfiles..."
 
-stow --dir "$(pwd)" --target "$HOME" hypr ; stow --dir "$(pwd)" --target "$HOME" ghostty ; stow --dir "$(pwd)" --target "$HOME" yazi ; stow --dir "$(pwd)" --target "$HOME" zsh ; stow --dir "$(pwd)" --target "$HOME" tmux
+stow --dir "$(pwd)" --target "$HOME" hypr
+stow --dir "$(pwd)" --target "$HOME" ghostty
+stow --dir "$(pwd)" --target "$HOME" yazi
+stow --dir "$(pwd)" --target "$HOME" zsh
+stow --dir "$(pwd)" --target "$HOME" tmux
 
 echo "Dotfiles installed. Reload your shell or restart apps if needed."
+
